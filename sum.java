c145514a115684381efc36189d34cc7123556c87
Anand Soni <anandsoni0403@gmail.com>
2024-03-21 00:02:23 -0700

    adding sum.java

---
+++ sum.java
@@ -0,0 +1,12 @@
+public class sum{
+
+    public static void main(String[] args){
+
+        int a = 1;
+        int b = 6;
+        int c = a+b;
+        System.out.print("Sum of a and b = "+ c);
+        
+    }
+    
+}
